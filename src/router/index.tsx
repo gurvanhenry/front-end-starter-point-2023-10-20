6d3c056e019c1f9c32e47cc478b200315450dba9
Gurvan Henry <gurvan.henry@gmail.com>
2023-10-20 09:51:21 +0200

    Add ts config for import @/ in project

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Home } from '../pages/Home';
-import { About } from '../pages/About';
+import { Home } from '@/pages/Home';
+import { About } from '@/pages/About';
 
 export const router = createBrowserRouter([
   {
