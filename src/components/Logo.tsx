3591191a4ca66a97e727f567d5238f81fb646d14
Gurvan Henry <gurvan.henry@gmail.com>
2023-10-20 10:18:00 +0200

    Add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+export function Logo() {
+  return (
+    <img
+      src="https://picsum.photos/200/100"
+      alt="random image"
+      style={{
+        width: 200,
+        height: 100,
+        backgroundColor: '#22109292',
+        borderRadius: '5%',
+      }}
+    />
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import { Logo } from '@/components/Logo';
+
 export function Home() {
   return (
     <div>
+      <Logo />
       <p>Home page</p>
       <p>
         go to <Link to="/about">about</Link> page
